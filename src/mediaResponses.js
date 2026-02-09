935744f4536501ad0139051eb3b75ae8727136e1
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2026-02-08 19:02:53 -0500

    streaming helper

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 //const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
+/*
 const getParty = (request, response) => {
     const file = path.resolve(__dirname, '../client/party.mp4');
     
@@ -50,10 +51,49 @@ const getParty = (request, response) => {
     });
     
 };
+*/
 
 //streaming helper
 const streamFile = (filePath, contentType, request, response) => {
-    
-}
+    fs.stat(filePath, (err, stats) => {
+        if(err){
+            if(err.code === 'ENOENT'){
+                response.writeHead(404);
+                return response.end('File not found');
+            }
+            response.writeHead(404);
+            return response.end('Server error');
+        }
+
+        let { range } = request.headers;
+        if(!range){
+            range = 'bytes=0-';
+        }
+
+        const [startStr, endStr] = range.replace(/bytes=/, '').split('-');
+        let start = parseInt(startStr, 10);
+        const total = stats.size;
+        const end = endStr ? parseInt(endStr, 10) : total - 1;
+
+        if(start > end){
+            start = 0;
+        }
+
+        const chunkSize = (end - start) + 1;
+
+        response.writeHead(206, {
+            'Content-Range': `bytes ${start}-${end}/${total}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunkSize,
+            'Content-Type': contentType
+        });
+
+
+        const stream = fs.createReadStream(filePath, { start, end});
+
+        stream.on('open', () => stream.pipe(response));
+        stream.on('error', (streamErr) => response.end(streamErr));
+    });
+};
 
 module.exports.getParty = getParty;
